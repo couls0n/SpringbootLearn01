92ba2e44a79d8c66b8280a4d096c1cc165ff6e13
Cou1son <14692714+cou1son@user.noreply.gitee.com>
2026-05-31 23:20:24 +0800

    最小demo

---
+++ src/main/java/com/example/moslearn/MosLearnDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.moslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosLearnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosLearnDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/moslearn/MosLearnDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.moslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosLearnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
